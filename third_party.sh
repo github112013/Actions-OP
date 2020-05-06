5117a696fd1d2e42dc49864f4c5aecf1960cf1ae
github112013 <44917863+github112013@users.noreply.github.com>
2020-05-06 11:59:34 +0800

    Update third_party.sh

--- third_party.sh
+++ third_party.sh
@@ -6,4 +6,4 @@
 #=================================================
 cd openwrt
 
-sed -i '$a\src-git rosy https://github.com/rosywrt/luci-theme-rosy' feeds.conf.default
+sed -i '$a\src-git rosy https://github.com/awesome-openwrt/luci-app-ssr-plus-mini.git' feeds.conf.default
